337e4ee5f6d55f2ce2b505facb5d510a76ac3fa0
souloper <cse2019026@rcciit.org.in>
2023-09-27 14:56:12 +0530

    styling issue

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -9,20 +9,20 @@ export default function Home() {
   const { user } = useClerk();
 
   if (!user) return redirect("/sign-in");
-//for server side preferences
-// import { currentUser } from "@clerk/nextjs";
-// import type { User } from "@clerk/nextjs/api";
- 
-// export default async function Home() {
-//   const user: User | null = await currentUser();
-//   if (!user) return redirect("/sign-in");
+  //for server side preferences
+  // import { currentUser } from "@clerk/nextjs";
+  // import type { User } from "@clerk/nextjs/api";
+
+  // export default async function Home() {
+  //   const user: User | null = await currentUser();
+  //   if (!user) return redirect("/sign-in");
 
   return (
     <main>
       <Header />
 
       <Board />
-      <Footer/>
+        <Footer />
     </main>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <footer className="py-4 text-center absolute bottom-0 w-full bg-gray-500/10 rounded-t-2xl">
+    <footer className="flex flex-col justify-between items-center min-h-[10vh] py-4 text-center lg:absolute lg:bottom-0 w-full bg-gray-500/10 rounded-t-2xl">
       <p className="text-gray-400">Copyright &copy; {new Date().getFullYear()} TaskMate</p>
     </footer>
   )
